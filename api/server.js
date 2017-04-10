0a172b8a6da32d2e54235f4fbaeb1d90fff86d86
timseo <gdurtyfive@gmail.com>
2017-04-10 15:37:34 -0700

    add public views dir and add bringin jwts bcrypt cors

--- api/server.js
+++ api/server.js
@@ -22,8 +22,38 @@ app.use('/api/drinks', drinkRoutes)
 // mounts userRoutes at /api/users
 app.use('/api/users', userRoutes)
 
+require('dotenv').config();
 
 //run the web server
 app.listen(port, function(){
 	console.log('Andre', port)
 })
+
+
+// for authentication
+
+function validateContentType(req, res, next) {
+  var methods = ['PUT', 'PATCH', 'POST'];
+  if (                                    // If the request is
+    methods.indexOf(req.method) !== -1 && // one of PUT, PATCH or POST, and
+    Object.keys(req.body).length !== 0 && // has a body that is not empty, and
+    !req.is('json')                       // does not have an application/json
+  ) {                                     // Content-Type header, then …
+    var message = 'Content-Type header must be application/json.';
+    res.status(400).json(message);
+  } else {
+    next();
+  }
+}
+
+// When there is a 401 Unauthorized, the repsonse shall include a header
+// WWW-Authenticate that tells the client how they must authenticate
+// their requests.
+function addFailedAuthHeader(err, req, res, next) {
+  var header = {'WWW-Authenticate': 'Bearer'};
+  if (err.status === 401) {
+    if (err.realm) header['WWW-Authenticate'] += ` realm="${err.realm}"`;
+    res.set(header);
+  }
+  next(err);
+}
