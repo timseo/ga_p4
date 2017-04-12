0ece5163630b3d6bb7aad5210e34983a7d1d1b9a
timseo <gdurtyfive@gmail.com>
2017-04-12 15:22:29 -0700

    maint

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express      = require('express'),
   	logger       = require('morgan'),
   	bodyParser	 = require('body-parser'),
   	mongoose     = require('mongoose'),
-  	// port         = process.env.PORT || 3000,
+  	port         = process.env.PORT || 8080,
     routes       = require('./config/routes.js')
 
 require('dotenv').config();
